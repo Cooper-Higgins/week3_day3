79ad239d1a5a76ef288949c305599c8163f7271c
Cooper-Higgins <jamie@supplantree.com>
2023-04-05 19:45:56 +0100

    Updated with CSS on index and order page(s)

--- models/order_list.py
+++ models/order_list.py
@@ -1,8 +1,8 @@
 from models.order import *
 
-order1 = Order(1, "Eddie Vedder", "01/03/2023", "1", "Little Oblivions", "Julien Baker", 15.00)
-order2 = Order(2, "Trent Reznor", "05/03/2023", "1", "Punisher", "Phoebe Bridgers", 20.00)
-order3 = Order(3, "James Hetfield", "02/04/2023", "1", "Historian", "Lucy Dacus", 25.00)
-order4 = Order(4, "Jerry Cantrell", "04/04/2023", "1", "The Record", "Boygenius", 30.00)
+order1 = Order(1, "Eddie Vedder", "01/03/2023", "1", "Little Oblivions", "Julien Baker", "£15.00")
+order2 = Order(2, "Trent Reznor", "05/03/2023", "1", "Punisher", "Phoebe Bridgers", "£20.00")
+order3 = Order(3, "James Hetfield", "02/04/2023", "1", "Historian", "Lucy Dacus", "£25.00")
+order4 = Order(4, "Jerry Cantrell", "04/04/2023", "1", "The Record", "Boygenius", "£30.00")
 
 orders = [order1, order2, order3, order4]
