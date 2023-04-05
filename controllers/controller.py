e578e8e5fc938783571941a4fbc97e2984f0e586
Cooper-Higgins <jamie@supplantree.com>
2023-04-05 15:25:54 +0100

    MVP & Extension complete and working as per brief

---
+++ controllers/controller.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+from models.order_list import orders
+
+@app.route('/')
+def index():
+    return render_template("index.html", title="CodeClan Records Admin", orders=orders)
+
+@app.route('/orders/<int:id>')
+def customer_order(id):
+    return render_template("order.html", title="Customer Order Full Details", order=orders[id])
---
+++ models/order.py
@@ -0,0 +1,10 @@
+class Order():
+
+    def __init__(self, id, customer_name, order_date, quantity, title, artist, price):
+        self.id = id
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.title = title
+        self.artist = artist
+        self.price = price
